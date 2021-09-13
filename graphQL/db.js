b37c09c0895923e09a06a41364aa44dfb4c72b5f
Ha young, Kim <jeremiah91@naver.com>
2021-09-13 12:54:52 +0900

    ----------  clear  ----------

--- graphQL/db.js
+++
@@ -1,9 +0,0 @@
-import fetch from "node-fetch";
-
-const API_URL = "https://yts.mx/api/v2/list_movies.json";
-
-export const getMovies = async (limit, rating) => {
-  const res = await fetch(`${API_URL}`);
-  const json = await res.json();
-  return json.data.movies;
-};
--- index.js
+++
@@ -1,9 +0,0 @@
-import { GraphQLServer } from "graphql-yoga";
-import resolvers from "./graphQL/resolvers";
-
-const server = new GraphQLServer({
-  typeDefs: "graphQL/schema.graphql",
-  resolvers,
-});
-
-server.start(() => console.log("GraphQL server is running now"));
