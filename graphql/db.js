03418e1e399aaac6a5ea675307bc964e496ab98a
Ha young, Kim <jeremiah91@naver.com>
2021-09-23 16:51:33 +0900

    feat : Wrap API with GraphQL

--- graphql/db.js
+++ graphql/db.js
@@ -1,49 +1,16 @@
-let movies = [
-  {
-    id: 1,
-    title: "Star Wars",
-    score: 4,
-  },
-  {
-    id: 2,
-    title: "Star Wars II",
-    score: 10,
-  },
-  {
-    id: 3,
-    title: "Star Wars III",
-    score: 9,
-  },
-  {
-    id: 4,
-    title: "Star Wars IV",
-    score: 6,
-  },
-];
+import fetch from "cross-fetch";
 
-export const getMovies = () => movies;
+const API_URL = "https://yts.mx/api/v2/list_movies.json?";
 
-export const getById = (id) => {
-  const filteredMovies = movies.filter((movie) => movie.id === id);
-  return filteredMovies[0];
-};
-
-export const addMovie = (title, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    title,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
-};
-
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
   }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
